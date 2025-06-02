41113cc11da0982ac7814fc71923b226a19ce85d
kebtsegaye <keburefantaye@gmail.com>
2025-06-01 18:34:22 -0600

    rest request setup for sessions

--- src/main/java/com/app/blep/controller/SessionsController.java
+++ src/main/java/com/app/blep/controller/SessionsController.java
@@ -1,4 +1,44 @@
 package com.app.blep.controller;
 
+import com.app.blep.model.Sessions;
+import com.app.blep.service.SessionsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/Sessions")
 public class SessionsController {
+    SessionsService sessionsService;
+
+    public SessionsController(SessionsService sessionsService) {
+        this.sessionsService = sessionsService;
+    }
+
+    @GetMapping
+    public List<Sessions> getAllSessions() {
+        return sessionsService.getSessions();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Sessions> getSessionById(@PathVariable int id) {
+        return sessionsService.getSessionById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+//    @PostMapping
+//    public Comments postComment(@PathVariable int userId, @PathVariable int postId,
+//                                @PathVariable String commentContent) {
+//        return commentsService.addComment(userId, postId, commentContent);
+//    }
+
+//    @DeleteMapping("/{id}/delete")
+//    public boolean deleteComment(@PathVariable int commentId) {
+//        return commentsService.deleteComment(commentId);
+//    }
+
+//    @PutMapping("{id}/update")
+//    public Comments updateComment(@PathVariable int commentId, @PathVariable int userId, @PathVariable int postId, @PathVariable String commentContent) {
+//        return commentsService.updateComment(commentId, userId, postId, commentContent);
+//    }
 }
--- src/main/java/com/app/blep/service/SessionsService.java
+++ src/main/java/com/app/blep/service/SessionsService.java
@@ -1,13 +1,25 @@
 package com.app.blep.service;
 
+import com.app.blep.model.Sessions;
 import com.app.blep.repository.SessionsRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class SessionsService {
-    private SessionsRepo sessionsRepo;
+    private final SessionsRepo sessionsRepo;
 
     public SessionsService(SessionsRepo sessionsRepo) {
         this.sessionsRepo = sessionsRepo;
+    }
+
+    public List<Sessions> getSessions() {
+        return sessionsRepo.findAll();
+    }
+
+    public Optional<Sessions> getSessionById(int id) {
+        return sessionsRepo.findById((long) id);
     }
 }
